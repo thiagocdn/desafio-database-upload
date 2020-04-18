a07cc6414852870ab75dd1cf92cabe5f513b52b4
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-04-18 15:59:01 -0300

    Desafio Concluído

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,4 @@
-import { EntityRepository, Repository } from 'typeorm';
-
+import { EntityRepository, Repository, getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -8,10 +7,53 @@ interface Balance {
   total: number;
 }
 
+interface CategoryDTO {
+  id: string;
+  title: string;
+}
+
+interface CreateTransactionDTO {
+  id: string;
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: CategoryDTO;
+}
+
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transactions = await transactionsRepository.find();
+    const income = transactions.reduce((total, transaction) => {
+      if (transaction.type === 'income') {
+        return total + Number(transaction.value);
+      }
+      return total;
+    }, 0);
+
+    const outcome = transactions.reduce((total, transaction) => {
+      if (transaction.type === 'outcome') {
+        return total + Number(transaction.value);
+      }
+      return total;
+    }, 0);
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
+  }
+
+  public async getTransactions(): Promise<CreateTransactionDTO[]> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transactions = transactionsRepository.find({
+      select: ['id', 'title', 'value', 'type'],
+      relations: ['category'],
+    });
+    return transactions;
   }
 }
 
